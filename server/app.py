4fd94c61387c1eac33941b12898252cfbdc471b4
kgrace61 <110522366+kgrace61@users.noreply.github.com>
2024-04-23 23:26:27 -0500

    completes lab

--- server/app.py
+++ server/app.py
@@ -21,7 +21,25 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    quake = Earthquake.query.filter_by(id=id).first()
+    if quake:
+        body = quake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
 
-
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def by_magnitude(magnitude):
+    quakes = []
+    for quake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quakes.append(quake.to_dict())
+    body = {'count' : len(quakes), 
+            'quakes': quakes
+            }
+    return make_response(body, 200)
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
